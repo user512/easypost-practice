ce2a9e3d0af5a3dc858d5a5db33ba375e77d2995
Tom Lee <tlee418@g.ucla.edu>
2015-10-27 18:44:59 -0700

    mvp with print function

--- app/assets/javascripts/packages.js
+++ app/assets/javascripts/packages.js
@@ -1,27 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-// $(document).ready(function () {
-//      $("#postageForm").submit(function (e) {
-//         e.preventDefault();
-
-//         var data = $(this);
-//         console.log(data);
-
-//         var buyPostage = $.ajax({
-//           url: "/easyposts",
-//           method: "post",
-//           data : data
-//         })
-
-//         buyPostage.done(function(responseData){
-//           console.log("success");
-//           console.log(responseData);
-//         });
-
-//         request.fail(function(responseData){
-//           console.log("Failed Ajax call");
-//         })
-//      });
-
-//  });
--- app/controllers/easyposts_controller.rb
+++ app/controllers/easyposts_controller.rb
@@ -4,45 +4,46 @@ class EasypostsController < ApplicationController
 
   def create
 
-begin
-    shipment = EasyPost::Shipment.create(
-    {
-      to_address: {
-        name: params[:recipient_name],
-        company: params[:recipient_company],
-        street1: params[:recipient_street_1],
-        street2: params[:recipient_street_2],
-        city: params[:recipient_city],
-        state: params[:recipient_state],
-        zip: params[:recipient_zip]
-        },
-        from_address: {
-          name: params[:sender_name],
-          company: params[:sender_company],
-          street1: params[:sender_street_1],
-          street2: params[:sender_street_2],
-          city: params[:sender_city],
-          state: params[:sender_state],
-          zip: params[:sender_zip],
-          phone: params[:sender_phone]
+    begin
+      shipment = EasyPost::Shipment.create(
+      {
+        to_address: {
+          name: params[:recipient_name],
+          company: params[:recipient_company],
+          street1: params[:recipient_street_1],
+          street2: params[:recipient_street_2],
+          city: params[:recipient_city],
+          state: params[:recipient_state],
+          zip: params[:recipient_zip]
           },
-          parcel: {
-            length: params[:parcel_length].to_i,
-            width: params[:parcel_width].to_i,
-            height: params[:parcel_height].to_i,
-            weight: params[:parcel_weight].to_i
+          from_address: {
+            name: params[:sender_name],
+            company: params[:sender_company],
+            street1: params[:sender_street_1],
+            street2: params[:sender_street_2],
+            city: params[:sender_city],
+            state: params[:sender_state],
+            zip: params[:sender_zip],
+            phone: params[:sender_phone]
+            },
+            parcel: {
+              length: params[:parcel_length].to_i,
+              width: params[:parcel_width].to_i,
+              height: params[:parcel_height].to_i,
+              weight: params[:parcel_weight].to_i
+            }
           }
-        }
-        )
+          )
 
       shipment.buy(
         :rate => shipment.lowest_rate(carriers = ['USPS']))
+      @postage_label = shipment.postage_label.label_url
+      render template: "packages/index"
 
-    @postage_url = shipment.postage_label.label_url
-  rescue Exception
-    flash.notice = "PLEASE INPUT CORRECT ADDRESS TO PRINT POSTAGE LABEL"
-    render template: "packages/new"
+    rescue Exception
+      flash.notice = "PLEASE INPUT CORRECT ADDRESS TO PRINT POSTAGE LABEL"
+      render template: "packages/new"
 
-  end
+    end
   end
 end
