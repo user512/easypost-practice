83300b38407a70bca3cb219928b95df928817169
Tom Lee <tlee418@g.ucla.edu>
2015-11-06 16:54:02 -0800

    add zip validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,17 @@
 class Address < ActiveRecord::Base
   belongs_to :package
-  validates :name, :street1, :city, :state, :zip, presence: true
+  has_one :package
+  validates :name, :street1, :city, :state, presence: true
+  validates_inclusion_of :zip, :in => 10000..99999
+
+  # def format_address
+  #   self.name = self.name.split.map(&:capitalize).join(' ')
+  #   self.street1 = self.street1.split.map(&:capitalize).join(' ')
+  #   self.street2 = self.street2.split.map(&:capitalize).join(' ')
+  #   self.city = self.city.split.map(&:capitalize).join(' ')
+  #   self.state = self.state.split.map(&:capitalize).join(' ')
+  # end
+
+
+
 end
